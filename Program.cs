ad300833e9299715e09b94205357b4fcc1173cd2
Bogdan Mihai <bogdanmihai@Oanas-MacBook.local>
2016-10-12 21:32:43 +0300

    Initial program to test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsTestsApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello world";
+        }
+    }
+}
