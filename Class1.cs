ae6c412bbacf05a347cc8f63503b570e791f9c4c
Bogdan Mihai <bogdanmihai@Oanas-MacBook.local>
2016-10-24 23:20:55 +0300

    Changd the implementation for the program cs and made the test to run

--- Class1.cs
+++ Class1.cs
@@ -22,7 +22,7 @@ namespace JenkinsTestsApp
         [Fact]
         public void PassingTest2()
         {
-            Assert.Equal("Hello world1", Program.CreateMessage());
+            Assert.Equal("Hello world1fortest", Program.CreateMessage());
         }
 
         int Add(int x, int y)
