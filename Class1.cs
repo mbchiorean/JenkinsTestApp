85a14f9fb23451794b47fae2e2e2d7da401b1839
Bogdan Mihai <bogdanmihai@oanas-macbook.yonder.local>
2016-12-15 16:38:28 +0200

    Fixed the tests

--- Class1.cs
+++ Class1.cs
@@ -23,7 +23,7 @@ namespace JenkinsTestsApp
         [Fact]
         public void PassingTest2()
         {
-            Assert.Equal("Hello world1fortest", Program.CreateMessage());
+            Assert.Equal("Hello world1fortest+1", Program.CreateMessage());
         }
 
         int Add(int x, int y)
